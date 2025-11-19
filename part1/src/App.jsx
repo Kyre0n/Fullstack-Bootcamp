f8a9084e7e1c1a9ffd1b516b4efe32937420598e
Kyre0n <samu21578@gmail.com>
2025-11-19 21:50:00 +0100

    He hecho toda la parte 1 del bootcamp menos los ejercicios finales, que es lo que haré la próxima vez que me ponga.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-
 const Hello = (props) => {
   const now = new Date()
   const a = 10
@@ -37,6 +36,21 @@ const Edad = ({name, age}) => {
   )
 }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const Display = ({counter}) => <div>{counter}</div>
 
 const Button = ({ onClick, text}) => <button onClick={onClick}>{text}</button>
@@ -45,20 +59,53 @@ const App = () => {
   const [ counter, setCounter ] = useState(0)
   console.log('rendering with counter value', counter)
 
-  const increaseByOne= () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0       //Dejo esta forma en vez de usar dos useState distintos para practicar el uso de objetos en el state pero no es lo más recomendable según el bootcamp ya que dice que "no hay ningún beneficio aparente y la aplicación resultante es mucho más compleja."
+  })
+
+   const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    const newClicks = {
+      ...clicks,
+      left: clicks.left + 1,
+    }
+    setClicks(newClicks)
+    setAll(allClicks.concat('L'))
   }
 
-  const decreaseByOne = () => {
-    setCounter(counter - 1)
-    console.log('decreasing, value before', counter)
+  const handleRightClick = () => {
+    const newClicks = {
+      ...clicks,
+      right: clicks.right + 1
+    }
+    setClicks(newClicks)
+        setAll(allClicks.concat('R'))
   }
 
-  const setToZero = () => {
-    setCounter(0)
-    console.log('resetting to zero, value before', counter)
+  const createHandleClick = ({value}) => {
+    if (value === 1) {
+      const handleIncrement = () => {
+        console.log('incrementing, value before', counter)
+        setCounter(counter + value)
+      }
+      return handleIncrement
+    } else if (value === -1) {
+      const handleDecrement = () => {
+        console.log('decrementing, value before', counter)
+        setCounter(counter + value)
+      }
+      return handleDecrement
+    } else {
+        const handleReset = () => {
+        console.log('resetting to zero, value before', counter)
+        setCounter(0)
+      }
+      return handleReset
+    }
   }
+
+
 
   /*
   setTimeout(             // No se debe poner un setTimeout ni setInterval aquí porque se ejecuta en cada renderizado y hace que de problemas.
@@ -78,17 +125,29 @@ const App = () => {
       <Edad name='George' age={26 + 10}/>
       <Display counter={counter}/>
       <Button
-        onClick={increaseByOne}
+        onClick={ createHandleClick({value: 1}) }
         text='plus'
       />
       <Button
-        onClick={setToZero}
+        onClick={ createHandleClick({value: 0}) }
         text='zero'
       />
-      <Button        
-        onClick={decreaseByOne}        
-        text='minus'      
-      />  
+      <Button
+        onClick={ createHandleClick({value: -1}) }
+        text='minus'
+      />
+      <p/>
+      {clicks.left}
+      <Button
+        onClick={handleLeftClick}
+        text="Left"
+      />
+      <Button
+        onClick={handleRightClick}
+        text="Right"
+      />
+      {clicks.right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
