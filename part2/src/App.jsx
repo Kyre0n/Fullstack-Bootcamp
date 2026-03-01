101f7c55af5be6265f1db4e4118d7bbfa6aaba78
Kyre0n <Samu21578@gmail.com>
2026-03-01 19:28:47 +0100

    Ejercicio 2.11 hecho

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,13 +1,53 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
 
+    useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  /*                          Esta función es la misma que el useEffect de arriba pero separado en una función para ver más claramente que pide dos argumentos, una función y un array de dependencias
+  const hook = () => {                      
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}
+
+useEffect(hook, [])
+*/
+
+/*                            Esta es otra forma de escribir lo mismo pero haciendo una funcion para el event handler para ver más claramente que es cada cosa, aunque es un poco más larga que la anterior
+useEffect(() => {
+  console.log('effect')
+
+  const eventHandler = response => {
+    console.log('promise fulfilled')
+    setNotes(response.data)
+  }
+
+  const promise = axios.get('http://localhost:3001/notes')
+  promise.then(eventHandler)
+}, [])
+*/
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
