6a118a91d8d917f7a0455a11a0cefaeff17e5635
Kyre0n <Samu21578@gmail.com>
2026-03-04 01:35:24 +0100

    Ejercicio 2.18 hecho

---
+++ part2/countrydata/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import countryService from './services/countryService'
+import Countries from './components/Countries'
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState('')
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(countries => setCountries(countries))
+  }, [])
+
+  const handleCountryChange = Event => {
+    setSelectedCountry(Event.target.value)
+  }
+
+
+
+  return (
+    <div>
+      find countries <input value={selectedCountry} onChange={handleCountryChange} />
+      <Countries countries={countries} selectedCountry={selectedCountry} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countrydata/src/components/Countries.jsx
@@ -0,0 +1,36 @@
+const Countries = ({ countries = [], selectedCountry = '' }) => {
+    const query = selectedCountry.toLowerCase()
+    const result = countries.filter(country =>
+            country.name.common.toLowerCase().includes(query)
+        )
+    if (result.length > 10) {
+        return (
+            <div><p>Too many matches, specify another filter</p></div>
+        )
+    }
+
+    if (result.length === 1) {
+        return (
+            <div>
+                <h2>{result[0].name.common}</h2>
+                <p>Capital: {result[0].capital.map(capital => `${capital} `)}</p>
+                <p>Area: {result[0].area}</p>
+                <h2>Languages</h2>
+                <ul>
+                {Object.values(result[0].languages).map(language => <li key={language}>{language}</li>)}
+                </ul>
+                <img src={result[0].flags.png} alt={`Flag of ${result[0].name.common}`} />
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            {result.map(country => (
+                <p key={country.cca3}>{country.name.common}</p>
+            ))}
+        </div>
+    )
+}
+
+export default Countries
